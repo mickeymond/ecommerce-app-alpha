8672ad318c3d0066f3fa04f08e3283bf25d4a7eb
teyvi <44750503+teyvi@users.noreply.github.com>
2024-03-05 12:21:04 +0000

    created divs for the navbar items

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,7 +3,15 @@ export default function Navbar() {
     return (
         <>
             <p>More code</p>
-            <p>Navbar goes here</p>
+            <div>
+                <div>
+                    logo goes here
+                </div>
+
+                <div>
+                    home, shop, ca
+                </div>
+            </div>
         </>
     );
 }
