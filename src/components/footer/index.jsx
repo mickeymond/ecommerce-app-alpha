585dd7a7b403ea5fe8bb82ecd3abd09448bcc005
jenniferagbeli <agbelij12@gmail.com>
2024-03-06 00:27:44 +1200

    edited the footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,13 +2,29 @@
 export default function Footer() {
     return(
     <>
+        
+        <div className="flex flex-col items-center mt-80">
+        <ul className="flex justify-center space-x-4">
+          <li>
+            <a href="#" className="text-black-300 hover:text-white">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-black-300 hover:text-white">
+              Catalogue
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-black-300 hover:text-white">
+              Contact Us
+            </a>
+          </li>
+        </ul>
+        <p className="mt-4">&copy; 2024 Alpha Ecommerce. All rights reserved.</p>
+      </div>
 
-    <p>&copy; 2024 Globedam Ltd. All rights reserved.</p>
-          <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
-          </ul>
+    
 
     </>
     )
