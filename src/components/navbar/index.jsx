81c45c218ed8bbaee7d25fd3e6a277730d022abb
teyvi <44750503+teyvi@users.noreply.github.com>
2024-03-06 16:18:42 +0000

    worked on the spacing , padding and margin for the navbar element

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,12 +1,18 @@
 
+import { ShoppingCartIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import Avatar from '../../assets/images/profileAvatar.svg';
+
+
 export default function Navbar() {
     return (
         <>
             <p>More code</p>
-            <div className=" grid grid-rows-1 border-2 bg-gray-100 p-4">
+            <div className=" grid grid-rows-1  bg-gray-300 p-4">
                 <div className="flex  justify-between">
-                    <div className="text-3xl font-bold underline">
-                        logo
+                    <div className="text-5xl font-extrabold ...">
+                        <span>
+                            LOGO
+                        </span>
                     </div>
 
                     <div className="relative">
@@ -16,22 +22,29 @@ export default function Navbar() {
                                 type='search'
                                 id='search'
                                 placeholder="Search Product by name or category"
-                                className="border-none relative  px-20 py-2 rounded-s-lg"
+                                className="border-none relative  rounded-s-lg w-96 px-4 py-2"
                             />
                             <button
                                 dir="rtl"
-                                className="bg-gradient-to-r from-cyan-500 to-blue-500 rounded-s-lg font-semibold text-white px-2 py-2"
+                                className="bg-gradient-to-r from-cyan-500 to-blue-500 rounded-s-lg font-semibold text-white px-4 py-2 fixed"
                                 onClick='search()'
                             >
-                                Search
+                                <MagnifyingGlassIcon className="h-6 w-5 text-white relative" />
                             </button>
 
                         </form>
                     </div>
-                    <div>
-                        account
-                        help
-                        cart
+                    <div className=" flex space-x-8 px-3 py-2">
+                        <div>
+                            <img src={Avatar} className="h-7 w-7" alt="Profile Avatar" />
+                        </div>
+
+                        <div className="px-5 center">
+                            <ShoppingCartIcon className="h-7 w-7 text-blue-500" />
+                        </div>
+                        <div className="rounded-lg text-black font-large hover:bg-cyan-800 hover:text-white width-full">
+                            Help
+                        </div>
                     </div>
 
                 </div>
