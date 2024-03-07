c9fb40590d6d622bc229b16af355aa60c3985658
fredericka <frederickatettey10@gmail.com>
2024-03-07 11:16:07 +0000

    new changes on footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,59 +1,83 @@
 import React from 'react';
-import { Typography } from "@material-tailwind/react";
+import {
+  FaFacebook,
+  FaGithub,
+  FaInstagram,
+  FaTwitter,
+  FaTwitch,
+} from "react-icons/fa";
 
+const sections = [
+  {
+    title: "Solutions",
+    items: ["Marketing", "Analytics", "Commerce", "Data", "Cloud"],
+  },
+  {
+    title: "Support",
+    items: ["Pricing", "Documentation", "Guides", "API Status"],
+  },
+  {
+    title: "Company",
+    items: ["About", "Blog", "Jobs", "Press", "Partners"],
+  },
+  {
+    title: "Legal",
+    items: ["Claims", "Privacy", "Terms", "Policies", "Conditions"],
+  },
+];
+
+
+const items = [
+  { name: "Facebook", icon: FaFacebook, link: "https://www.facebook.com/" },
+  { name: "Instagram", icon: FaInstagram, link: "https://www.instagram.com/" },
+  { name: "Twitter", icon: FaTwitter, link: "https://twitter.com/" },
+  { name: "Twitch", icon: FaTwitch, link: "https://www.twitch.tv/" },
+  { name: "Github", icon: FaGithub, link: "https://github.com/" },
+];
 export default function Footer() {
+
   return ( 
-    <footer className="w-full bg-green p-8">
-      <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-green text-center md:justify-between">
-        <img src="https://docs.material-tailwind.com/img/logo-ct-dark.png" alt="logo-ct" className="w-10" />
-        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8 bg-green">
-          <li>
-            <Typography
-              as="a"
-              href="#"
-              color="blue-gray"
-              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
-            >
-              About Us
-            </Typography>
-          </li>
-          <li>
-            <Typography
-              as="a"
-              href="#"
-              color="blue-gray"
-              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
-            >
-              License
-            </Typography>
-          </li>
-          <li>
-            <Typography
-              as="a"
-              href="#"
-              color="blue-gray"
-              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
-            >
-              Contribute
-            </Typography>
-          </li>
-          <li>
-            <Typography
-              as="a"
-              href="#"
-              color="blue-gray"
-              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
-            >
-              Contact Us
-            </Typography>
-          </li>
-        </ul>
+    <footer className="bg-black text-white p-10">
+     <div className="w-full mt-24 bg-slate-900 text-gray-300 py-y px-2">
+      <div className="max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8">
+        {sections.map((section, index) => (
+          <div key={index}>
+            <h6 className="font-bold uppercase pt-2">{section.title}</h6>
+            <ul>
+              {section.items.map((item, i) => (
+                <li key={i} className="py-1 text-gray-500 hover:text-white">
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+
+        <div className="col-span-2 pt-8 md:pt-2">
+          <p className="font-bold uppercase">Subscribe to our newsletter</p>
+          <p className="py-4">
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
+          <form className="flex flex-col sm:flex-row">
+            <input
+              className="w-full p-2 mr-4 rounded-md mb-4"
+              type="email"
+              placeholder="Enter email.."
+            />
+            <button className="p-2 mb-4">Subscribe</button>
+          </form>
+        </div>
       </div>
-      <hr className="my-8 border-blue-gray-50" />
-      <Typography color="green-white" className="text-center font-normal bg-green">
-        &copy; 2023 Material Tailwind
-      </Typography>
+
+      <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500">
+        <p className="py-4">2022 Workflow, LLC. All rights reserved</p>
+        <div className="flex justify-between sm:w-[300px] pt-4 text-2xl">
+          {items.map((x, index) => {
+            return <x.icon key={index} className="hover:text-white" />;
+          })}
+        </div>
+      </div>
+    </div>
     </footer>
-   
   )
 }
