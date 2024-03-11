c4eadcf3155c81d1174ce105ac543b64354a0f69
abdulazeezcoding <abdulazizmohammed5103@gmail.com>
2024-03-11 13:36:18 +0000

    catalogue creation

--- src/pages/catalogue/index.jsx
+++ src/pages/catalogue/index.jsx
@@ -1,11 +1,27 @@
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
+import { productItems } from "./products-json";
 
 export default function Catalogue() {
     return (
         <>
             <Navbar />
-            <p>Catalogue</p>
+        <div className="grid-cols-4 grid-rows-4 gap-3"> 
+        {productItems.map((product) => (
+            <div key={product.id} >
+                 <div className="product-card">
+                    {/* add width of 100% to images */}
+                    <img src={product.image_url} alt="T-shirts" />
+                    <h2>{product.name}</h2>
+                    <p className="price">GHC {product.price}</p>
+                    <p className="seller">{product.seller}</p>
+                    <p><button type="submit">Add to Cart</button></p>
+                </div> 
+            </div>
+        ))}
+
+        </div>
+
             <Footer />
         </>
     );
---
+++ src/pages/catalogue/products-json.js
@@ -0,0 +1,153 @@
+
+export const productItems = [
+    {
+        id: 1,
+        name: "Customizable T-Shirts",
+        image_url: "../src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      },
+      {
+        id: 2,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 3,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 4,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 5,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 6,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 7,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 8,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 9,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 10,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 11,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 12,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      }, {
+        id: 13,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      },
+      {
+        id: 14,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      },
+      {
+        id: 15,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      },
+      {
+        id: 16,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      },
+      {
+        id: 17,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      },
+      {
+        id: 18,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      },
+      {
+        id: 19,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      },
+      {
+        id: 20  ,
+        name: "Customizable T-Shirts",
+        image_url: "./src/assets/images/shirt.jpg",
+        price: 34.99,
+        description: "Cool customizable tops for everyday use.",
+        seller: "Kantamanto"
+      },
+
+];
