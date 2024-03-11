7b8d6aa4f0f77ab2e0f7f775a7b9351cf8c759d6
JoanaQuadzie <joanaq03@mail.com>
2024-03-10 17:00:57 -0700

    added 2 images

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,34 +1,67 @@
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
+import cooker_1 from "../../assets/images/cooker_1.jpg";
 
 export default function Cart() {
-    return (
-        <>
-            <Navbar />
-            <section className="p-4 bg-gray-300 h-screen">
-                <article className="bg-white p-4 rounded-lg shadow-md">
-                    <h2 className="text-2xl font-bold mb-4">Cart(3)</h2>
-
-                    <div className="flex gap-4 " >
-                        <figure className="">
-                            <img src="https://via.placeholder.com/150" alt="item-name" />
-                        </figure>
-                        <div>
-                            <div className="flex justify-between gap-4 items-center ">
-                                <h3 className="font-medium leading-10 text-xl">Sport Short Sleeve T-shirt & Shorts Set</h3>
-                                <span className="font-semibold text-xl" >GHS93.00</span>
-                            </div>
-                            <p className="leading-10"><span>Seller:&nbsp;kili Brand Shop</span> | <span>Size: M</span></p>
-                            <span className="bg-indigo-500 text-white p-1 rounded-md">In Stock</span>
-                        </div>
-                    </div>
-                </article>
-            </section>
-
-
-            <Footer />
-        </>
-    );
+  const savedItems = [
+    {
+      id: 1,
+      item_name: "Sport Short Sleeve T-shirt & Shorts Set",
+      image: cooker_1,
+      price: 93.0,
+      seller: "Kili Brand Shop",
+      size: ["S", "M", "L", "XL"],
+      availability_status: true,
+    },
+    {
+      id: 2,
+      item_name: "Nasco NASGS-K1CSS Gas Cooker",
+      image: cooker_1,
+      price: 139.0,
+      seller: "Kili Brand Shop",
+      size: ["S", "M", "L", "XL"],
+      availability_status: false
+    },
+  ];
+  return (
+    <>
+      <Navbar />
+      <section className="p-4 bg-gray-300 h-screen">
+        <article className="bg-white p-4 rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold mb-4">Cart({savedItems.length})</h2>
+          {savedItems.map((item) => {
+            return (
+              <div keys={item.id} className="flex gap-4 mb-4 border-b-4 pb-4">
+                <figure className="">
+                  <img
+                    src={item.image}
+                    alt={item.item_name}
+                    style={{ height: 100, width: 100 }}
+                  />
+                </figure>
+                <div>
+                  <div className="flex justify-between gap-4 items-center ">
+                    <h3 className="font-medium leading-10 text-xl">
+                      item.item_name
+                    </h3>
+                    <span className="font-semibold text-xl">
+                      GHS{item.price}{" "}
+                    </span>
+                  </div>
+                  <p className="leading-10">
+                    <span>Seller:&nbsp;{item.seller}</span> |{" "}
+                    <span>Size: {item.size[1]} </span>
+                  </p>
+                  <span className= {` ${item.availability_status ? "bg-indigo-500" : "bg-red-500"} text-white p-1 rounded-md`}>
+                   {item.availability_status ? "In Stock": "Sold Out"}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </article>
+      </section>
+      <Footer />
+    </>
+  );
 }
-
-
